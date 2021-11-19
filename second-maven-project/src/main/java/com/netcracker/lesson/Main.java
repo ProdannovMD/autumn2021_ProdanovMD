889dcdb3882fcebd9773a3ff31015ee55ca157e9
ProdanovMD <prodanovmd@gmail.com>
2021-11-19 19:24:31 +0400

    maven lesson

---
+++ second-maven-project/src/main/java/com/netcracker/lesson/Main.java
@@ -0,0 +1,12 @@
+package com.netcracker.lesson;
+
+import com.netcracker.lesson.Calculator;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Calculator.add(3, 4));
+        System.out.println(Calculator.subtract(10, 6));
+        System.out.println(Calculator.multiply(6, 7));
+        System.out.println(Calculator.divide(15, 3));
+    }
+}
